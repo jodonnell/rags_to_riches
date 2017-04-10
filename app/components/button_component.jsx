092515e65262dc49be02bee2da618fa30d5db418
Jacob O'Donnell <jacobodonnell@gmail.com>
2017-04-09 22:54:35 -0400

    adding animation

--- app/components/button_component.jsx
+++ app/components/button_component.jsx
@@ -1,26 +1,36 @@
-import styles from '../assets/styles/interactive.css';
+import styles from '../assets/styles/button.css';
 import React from 'react';
 
 class ButtonComponent extends React.Component {
     constructor(props) {
         super(props);
 
-        this.active = false;
+        this.state = {active: true};
         this.handleClick = this.handleClick.bind(this);
     }
 
     render() {
-        return (<div data-name={this.props.name} className={styles.button} onClick={this.handleClick}>{this.props.text}</div>);
+        if (this.state.active) {
+            return (<div data-name={this.props.name} className={styles.active} onClick={this.handleClick}>{this.props.text}</div>);
+        }
+        else {
+            const animateStyles = {animationDuration: (this.cooldown / 1000) + 's'};
+            return (<div data-name={this.props.name} className={styles.inactive}><div style={animateStyles} className={styles.animate}></div><div>{this.props.text}</div></div>);
+        }
     }
 
     handleClick() {
-        if (this.active) {
-            return;
-        }
-
         this.props.handleClick();
-        setTimeout(() => {this.active = false;}, parseInt(this.props.cooldown));
-        this.active = true;
+        setTimeout(() => {this.resetCooldown();}, this.cooldown);
+        this.setState({active: false});
+    }
+
+    resetCooldown() {
+        this.setState({active: true});
+    }
+
+    get cooldown() {
+        return parseInt(this.props.cooldown);
     }
 }
 
